780bb478e8023e004ae651709c49aa165e464a55
Cole Willsea <coleww@gmail.com>
2015-08-09 18:48:34 -0700

    ,arveloous

--- www/demo.js
+++ www/demo.js
@@ -36,27 +36,123 @@ function tapEnd (x, y, force) {
   drone.filter.frequency.value = y * 1000 + 100
 }
 
-var notes = ['146.83', '174.61', '220.0', '261.63', '293.67', '349.23', '440.0']
+function redraw (x, y, force) {
+  var ping = document.getElementById('ping')
+  ping.style.top = (x * window.innerHeight) + 'px'
+  ping.style.left = (y * window.innerWidth) + 'px'
 
+  force = force !== undefined ? force : 0.5
+  ping.style.width = force * 250
+  ping.style.height = force * 250
+
+}
+
+var notes = ['146.83', '174.61', '220.0', '261.63', '293.67', '349.23', '440.0']
+var colors = ['red', 'lightgreen', 'blue', 'orange', 'yellow', 'magenta', 'pink']
+var backs = ['green', 'lightblue', 'plum', 'maroon', 'beige', 'brown', 'black']
 window.setInterval(function () {
-  drone.source.frequency.value = notes[~~(Math.random() * notes.length)]
+  var note = notes[~~(Math.random() * notes.length)]
+  drone.source.frequency.value = note
+
+
+  var pairs = [mapRange(wobbler.delay.delayTime.value, 0, 0.25, 0, 1),
+  mapRange(drone.lowFilter.frequency.value, 0, 1000, 0, 1),
+
+  mapRange(wobbler.lfoGain.gain.value, 0, 1000, 0, 1),
+  mapRange(drone.filter.frequency.value, 0, 1000, 0, 1),
+
+  mapRange(wobbler.lfo.frequency.value, 0, 5, 0, 1),
+  mapRange(wobbler.delay.delayTime.value, 0, 0.25, 0, 1),
+
+  mapRange(drone.lowFilter.frequency.value, 0, 1000, 0, 1),
+  mapRange(drone.filter.frequency.value, 0, 1000, 0, 1),
+
+  mapRange(wobbler.lfo.frequency.value, 0, 5, 0, 1),
+  mapRange(wobbler.lfoGain.gain.value, 0, 1000, 0, 1),
+
+
+
+  mapRange(drone.lowFilter.frequency.value, 0, 1000, 0, 1),
+  mapRange(wobbler.delay.delayTime.value, 0, 0.25, 0, 1),
+
+
+  mapRange(drone.filter.frequency.value, 0, 1000, 0, 1),
+  mapRange(wobbler.lfoGain.gain.value, 0, 1000, 0, 1),
+
+
+  mapRange(wobbler.delay.delayTime.value, 0, 0.25, 0, 1),
+  mapRange(wobbler.lfo.frequency.value, 0, 5, 0, 1),
+
+
+  mapRange(wobbler.lfo.frequency.value, 0, 5, 0, 1),
+  mapRange(drone.lowFilter.frequency.value, 0, 1000, 0, 1),
+
+  mapRange(drone.filter.frequency.value, 0, 1000, 0, 1),
+  mapRange(wobbler.lfoGain.gain.value, 0, 1000, 0, 1),
+
+
+  mapRange(drone.filter.frequency.value, 0, 1000, 0, 1),
+  mapRange(drone.lowFilter.frequency.value, 0, 1000, 0, 1),
+
+
+  mapRange(drone.filter.frequency.value, 0, 1000, 0, 1),
+  mapRange(wobbler.lfoGain.gain.value, 0, 1000, 0, 1),
+
+  mapRange(wobbler.lfo.frequency.value, 0, 5, 0, 1),
+  mapRange(wobbler.delay.delayTime.value, 0, 0.25, 0, 1),
+
+  mapRange(drone.lowFilter.frequency.value, 0, 1000, 0, 1),
+  mapRange(drone.filter.frequency.value, 0, 1000, 0, 1),
+
+  mapRange(wobbler.lfo.frequency.value, 0, 5, 0, 1),
+  mapRange(wobbler.lfoGain.gain.value, 0, 1000, 0, 1),
+
+  mapRange(drone.lowFilter.frequency.value, 0, 1000, 0, 1),
+  mapRange(drone.filter.frequency.value, 0, 1000, 0, 1),
+
+  mapRange(wobbler.lfo.frequency.value, 0, 5, 0, 1),
+  mapRange(wobbler.delay.delayTime.value, 0, 0.25, 0, 1),
+
+  mapRange(wobbler.lfoGain.gain.value, 0, 1000, 0, 1),
+  mapRange(wobbler.delay.delayTime.value, 0, 0.25, 0, 1),
+
+
+  mapRange(drone.filter.frequency.value, 0, 1000, 0, 1),
+  mapRange(wobbler.lfo.frequency.value, 0, 5, 0, 1),
+
+  mapRange(wobbler.lfoGain.gain.value, 0, 1000, 0, 1),
+  mapRange(drone.lowFilter.frequency.value, 0, 1000, 0, 1)
+]
+
+  var pieces = document.querySelectorAll(".piece")
+  for(var i = 0; i < pieces.length - 1; i++){
+    console.log(pairs[i * 2], notes.indexOf(note))
+    pieces[i].style.top = (pairs[(i * 2) + 1] * window.innerHeight / 2.5) + 'px'
+    pieces[i].style.left = (pairs[(i * 2)] * window.innerWidth / 25) + 'px'
+    pieces[i].style.backgroundColor = colors[(notes.indexOf(note) + i) % colors.length]
+  }
+  document.getElementById("bar").style.backgroundColor = backs[(notes.indexOf(note) + pieces.length) % colors.length]
+  // use the values of all the variable things to draw something?
 }, 2000)
 
 if ('ontouchstart' in document.documentElement) {
   document.getElementById("foo").addEventListener('touchstart', function (e) {
     var touch = e.touches.item(0)
+    redraw(mapRange(touch.clientX, 0, window.innerWidth, 0, 1), mapRange(touch.clientY, 0, window.innerHeight, 0, 1), touch.force)
     tapStart(mapRange(touch.clientX, 0, window.innerWidth, 0, 1), mapRange(touch.clientY, 0, window.innerHeight, 0, 1), touch.force)
   }, false)
 
   document.getElementById("foo").addEventListener('touchmove', function (e) {
     var touch = e.touches.item(0)
+    redraw(mapRange(touch.clientX, 0, window.innerWidth, 0, 1), mapRange(touch.clientY, 0, window.innerHeight, 0, 1), touch.force)
     dragChange(mapRange(touch.clientX, 0, window.innerWidth, 0, 1), mapRange(touch.clientY, 0, window.innerHeight, 0, 1), touch.force)
   }, false)
 
   document.getElementById("foo").addEventListener('touchend', function (e) {
     var touch = e.touches.item(0)
+    redraw(mapRange(touch.clientX, 0, window.innerWidth, 0, 1), mapRange(touch.clientY, 0, window.innerHeight, 0, 1), touch.force)
     tapEnd(mapRange(touch.clientX, 0, window.innerWidth, 0, 1), mapRange(touch.clientY, 0, window.innerHeight, 0, 1), touch.force)
   }, false)
 } else {
-  document.body.textContent = "FAIL"
+  document.getElementById("foo").textContent = "FAIL"
 }
